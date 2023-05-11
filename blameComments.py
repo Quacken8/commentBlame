d457f8993af4b9ed5d7299cdf3be9ce890819569
Ondra Janoška <o.janoska98@gmail.com>
2023-05-11 11:47:24 +0200

    formatting

--- blameComments.py
+++ blameComments.py
@@ -24,40 +24,46 @@ commentSymbols = {
 }
 
 
-def updateWithGitignore(directory, ignoredFileExtensions, ignoredFileNames, ignoredDirectories):
-        try:
-            with open(f'{directory}.gitignore') as f:
-                for line in f:
-                    if line[0] == '*':
-                        ignoredFileExtensions.append(line.strip()[1:])
-                    else:
-                        ignoredDirectories.append(line.strip())
-                        ignoredFileNames.append(line.strip())
-        except FileNotFoundError:
-            print("No .gitignore file found")
+def updateWithGitignore(
+    directory, ignoredFileExtensions, ignoredFileNames, ignoredDirectories
+):
+    try:
+        with open(f"{directory}.gitignore") as f:
+            for line in f:
+                if line[0] == "*":
+                    ignoredFileExtensions.append(line.strip()[1:])
+                else:
+                    ignoredDirectories.append(line.strip())
+                    ignoredFileNames.append(line.strip())
+    except FileNotFoundError:
+        print("No .gitignore file found")
 
 
 class File:
     comments = []
     numberOfLines = 0
+
     def __init__(self, name, path):
         self.name = name
         self.path = path
         self.comments = []
+
     def __str__(self):
         return self.name + " " + self.path
+
     def __repr__(self):
         return self.name + " " + self.path
-    
+
     def appendComment(self, comment):
         self.comments.append(comment)
-    
+
     def printComments(self):
         if len(self.comments) > 10:
             print(f"Too many comments to print ({len(self.comments)})")
             return
         for comment in self.comments:
             print(comment)
+
 
 def getCommentsFromFile(filePath: str, commentSymbols):
     """reads file and returns number of lines and number of comments
@@ -72,7 +78,7 @@ def getCommentsFromFile(filePath: str, commentSymbols):
 
     totalNumberOfLines = 0
 
-    file = File(filePath.split('/')[-1], filePath)
+    file = File(filePath.split("/")[-1], filePath)
 
     with open(file.path) as f:
         try:
@@ -81,21 +87,30 @@ def getCommentsFromFile(filePath: str, commentSymbols):
                 # remove strings
                 if '"' in line:
                     splitted = line.split('"')
-                    line = ' '.join(splitted[0::2])
+                    line = " ".join(splitted[0::2])
                 if "'" in line:
                     splitted = line.split("'")
-                    line = ' '.join(splitted[0::2])
+                    line = " ".join(splitted[0::2])
                 for symbol in commentSymbols:
                     if symbol in line:
                         splitted = line.split(symbol)
-                        file.appendComment(' '.join(splitted[1:]))
+                        file.appendComment(" ".join(splitted[1:]))
                         break
         except UnicodeDecodeError:
             pass
     file.numberOfLines = totalNumberOfLines
     return file
 
-def getAllFiles(directory:str, desiredExtensions, ignoredFileExtensions = [], ignoredFileNames = [], ignoredFilePrefixes = [], ignoredDirectories = [], ignoredDirectoryPrefixes = []):
+
+def getAllFiles(
+    directory: str,
+    desiredExtensions,
+    ignoredFileExtensions=[],
+    ignoredFileNames=[],
+    ignoredFilePrefixes=[],
+    ignoredDirectories=[],
+    ignoredDirectoryPrefixes=[],
+):
     """recursively gets all files in directory
 
     Args:
@@ -110,23 +125,23 @@ def getAllFiles(directory:str, desiredExtensions, ignoredFileExtensions = [], ig
     Returns:
         list: list of file objects
     """
-    
+
     file_paths = []
 
     for root, _, files in os.walk(directory):
         for filename in files:
-            if not any (ext in filename for ext in desiredExtensions):
+            if not any(ext in filename for ext in desiredExtensions):
                 continue
-            if any (ext in filename for ext in ignoredFileExtensions):
+            if any(ext in filename for ext in ignoredFileExtensions):
                 continue
             if filename in ignoredFileNames:
                 continue
             if filename[0] in ignoredFilePrefixes:
                 continue
             try:
-                if root.split('/')[-1] in ignoredDirectories:
+                if root.split("/")[-1] in ignoredDirectories:
                     continue
-                if root.split('/')[-1][0] in ignoredDirectoryPrefixes:
+                if root.split("/")[-1][0] in ignoredDirectoryPrefixes:
                     continue
             except IndexError:
                 pass
@@ -135,27 +150,41 @@ def getAllFiles(directory:str, desiredExtensions, ignoredFileExtensions = [], ig
 
     files = []
     for file in file_paths:
-        files.append(File(file.split('/')[-1], file))
+        files.append(File(file.split("/")[-1], file))
     return files
 
-def main(directory):
 
+def main(directory):
     totalNumberOfLines = 0
     totalNumberOfComments = 0
     ignoredFileExtensions = ["plugin", ".html"]
     ignoredFileNames = ["app_localizations.dart"]
-    ignoredFilePrefixes = ['.', '_']
+    ignoredFilePrefixes = [".", "_"]
     ignoredDirectories = []
-    ignoredDirectoryPrefixes = ['.', '_']
+    ignoredDirectoryPrefixes = [".", "_"]
 
     # recursively get all files in directory
-    
-    updateWithGitignore(directory, ignoredFileExtensions, ignoredFileNames, ignoredDirectories)
-    files = getAllFiles(directory, ".dart", ignoredFileExtensions, ignoredFileNames, ignoredFilePrefixes, ignoredDirectories, ignoredDirectoryPrefixes)
+
+    updateWithGitignore(
+        directory, ignoredFileExtensions, ignoredFileNames, ignoredDirectories
+    )
+    files = getAllFiles(
+        directory,
+        ".dart",
+        ignoredFileExtensions,
+        ignoredFileNames,
+        ignoredFilePrefixes,
+        ignoredDirectories,
+        ignoredDirectoryPrefixes,
+    )
     processedFiles = []
     for file in files:
         try:
-            processedFiles.append(getCommentsFromFile(file.path, commentSymbols["."+file.name.split('.')[-1]]))
+            processedFiles.append(
+                getCommentsFromFile(
+                    file.path, commentSymbols["." + file.name.split(".")[-1]]
+                )
+            )
         except KeyError:
             pass
 
@@ -168,10 +197,13 @@ def main(directory):
         totalNumberOfLines += file.numberOfLines
         totalNumberOfComments += len(file.comments)
 
-    print (f"Total number of lines: {totalNumberOfLines}")
-    print (f"Total number of comments: {totalNumberOfComments}")
-    print (f"Percentage of comments: {totalNumberOfComments/totalNumberOfLines*100:.2f} %")
+    print(f"Total number of lines: {totalNumberOfLines}")
+    print(f"Total number of comments: {totalNumberOfComments}")
+    print(
+        f"Percentage of comments: {totalNumberOfComments/totalNumberOfLines*100:.2f} %"
+    )
+
 
 if __name__ == "__main__":
-    directory = ('../cz.ikariera.fairapp/')
+    directory = "../cz.ikariera.fairapp/"
     main(directory)
